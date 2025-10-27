e05fc634f87a9f8e4ab46257e0a50cf965e1975e
Miguel Julio <fitfab@yahoo.com>
2025-10-27 12:19:18 -0400

    change copy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -16,6 +16,7 @@ export default function Home() {
       </p>
       <p>This is anothe deployment: ./.output/public</p>
       <p>change output to: vinxi/build</p>
+      <p>build folder: .output</p>
     </main>
   );
 }
